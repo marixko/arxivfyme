359dbd480438725782f8a5278699a2a42aa1c011
Lilianne Nakazono <marixko@gmail.com>
2022-10-20 14:51:47 +0200

    deleted library

--- arxivfyme.py
+++ arxivfyme.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 import nltk
 from nltk.stem import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 import streamlit as st
 
